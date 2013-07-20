fc73ca48da89c2ffa7a6a72acd15a71c41eb0dc1
Jamieson Becker <jamieson@jamiesonbecker.com>
2013-07-20 18:12:39 -0500

    fixed

--- unframework.js
+++ unframework.js
@@ -18,7 +18,43 @@ var background = {};
 for (var taskname in background){background[taskname]()}
 setTimeout(run_background_tasks, 250)})();
 function strict_safify(htm){return (htm||"").replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,'&#x27;').replace(/\//g,'&#x2F;').replace(/\n/g,'<br>')}
-function safify(htm){return strict_safify(htm).replace(/&lt;b&gt;/gi,"<b>".replace(/&lt;&#x2F;b&gt;/gi,"</b>".replace(/&lt;strong&gt;/gi,"<strong>".replace(/&lt;&#x2F;strong&gt;/gi,"</strong>".replace(/&lt;em&gt;/gi,"<em>".replace(/&lt;&#x2F;em&gt;/gi,"</em>".replace(/&lt;i&gt;/gi,"<i>".replace(/&lt;&#x2F;i&gt;/gi,"</i>".replace(/&lt;li&gt;/gi,"<li>".replace(/&lt;&#x2F;li&gt;/gi,"</li>".replace(/&lt;ul&gt;/gi,"<ul>".replace(/&lt;&#x2F;ul&gt;/gi,"</ul>".replace(/&lt;ol&gt;/gi,"<ol>".replace(/&lt;&#x2F;ol&gt;/gi,"</ol>".replace(/&lt;h1&gt;/gi,"<h1>".replace(/&lt;&#x2F;h1&gt;/gi,"</h1>".replace(/&lt;h2&gt;/gi,"<h2>".replace(/&lt;&#x2F;h2&gt;/gi,"</h2>".replace(/&lt;h3&gt;/gi,"<h3>".replace(/&lt;&#x2F;h3&gt;/gi,"</h3>".replace(/&lt;h4&gt;/gi,"<h4>".replace(/&lt;&#x2F;h4&gt;/gi,"</h4>".replace(/&lt;h5&gt;/gi,"<h5>".replace(/&lt;&#x2F;h5&gt;/gi,"</h5>".replace(/&lt;h6&gt;/gi,"<h6>".replace(/&lt;&#x2F;h6&gt;/gi,"</h6>".replace(/&lt;table&gt;/gi,"<table>".replace(/&lt;&#x2F;table&gt;/gi,"</table>".replace(/&lt;tr&gt;/gi,"<tr>".replace(/&lt;&#x2F;tr&gt;/gi,"</tr>".replace(/&lt;td&gt;/gi,"<td>".replace(/&lt;&#x2F;td&gt;/gi,"</td>".replace(/&lt;p&gt;/gi,"<p>".replace(/&lt;&#x2F;p&gt;/gi,"</p>".replace(/&lt;br&gt;/gi,"<br>"}
+function safify(htm) {
+return strict_safify(htm)
+.replace(/&lt;b&gt;/gi, " <b> ")
+.replace(/&lt;&#x2F;b&gt;/gi, " </b> ")
+.replace(/&lt;strong&gt;/gi, " <strong> ")
+.replace(/&lt;&#x2F;strong&gt;/gi, " </strong> ")
+.replace(/&lt;em&gt;/gi, " <em> ")
+.replace(/&lt;&#x2F;em&gt;/gi, " </em> ")
+.replace(/&lt;i&gt;/gi, " <i> ")
+.replace(/&lt;&#x2F;i&gt;/gi, " </i> ")
+.replace(/&lt;li&gt;/gi, " <li> ")
+.replace(/&lt;&#x2F;li&gt;/gi, " </li> ")
+.replace(/&lt;ul&gt;/gi, " <ul> ")
+.replace(/&lt;&#x2F;ul&gt;/gi, " </ul> ")
+.replace(/&lt;ol&gt;/gi, " <ol> ")
+.replace(/&lt;&#x2F;ol&gt;/gi, " </ol> ")
+.replace(/&lt;h1&gt;/gi, " <h1> ")
+.replace(/&lt;&#x2F;h1&gt;/gi, " </h1> ")
+.replace(/&lt;h2&gt;/gi, " <h2> ")
+.replace(/&lt;&#x2F;h2&gt;/gi, " </h2> ")
+.replace(/&lt;h3&gt;/gi, " <h3> ")
+.replace(/&lt;&#x2F;h3&gt;/gi, " </h3> ")
+.replace(/&lt;h4&gt;/gi, " <h4> ")
+.replace(/&lt;&#x2F;h4&gt;/gi, " </h4> ")
+.replace(/&lt;h5&gt;/gi, " <h5> ")
+.replace(/&lt;&#x2F;h5&gt;/gi, " </h5> ")
+.replace(/&lt;h6&gt;/gi, " <h6> ")
+.replace(/&lt;&#x2F;h6&gt;/gi, " </h6> ")
+.replace(/&lt;table&gt;/gi, " <table> ")
+.replace(/&lt;&#x2F;table&gt;/gi, " </table> ")
+.replace(/&lt;tr&gt;/gi, " <tr> ")
+.replace(/&lt;&#x2F;tr&gt;/gi, " </tr> ")
+.replace(/&lt;td&gt;/gi, " <td> ")
+.replace(/&lt;&#x2F;td&gt;/gi, " </td> ")
+.replace(/&lt;p&gt;/gi, " <p> ")
+.replace(/&lt;&#x2F;p&gt;/gi, " </p> ")
+.replace(/&lt;br&gt;/gi, " <br> ")}
 function unstrict_safify(htm){return (htm || "").replace(/<br>/g,'\n').replace(/&amp;/g,'&').replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&quot;/g,'"').replace(/&#x27;/g,"'").replace(/&#x2F;/g,'/')}
 $.fn.safify = function(evil){this.html(safify(evil));return this}
 $.fn.strict_safify = function(evil){this.html(strict_safify(evil));return this}
